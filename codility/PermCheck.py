18cab0c282b30aba2277227d0492b3833d0b5c67
mykola-avram <mykola.avram@gmail.com>
2020-04-29 16:38:04 +0200

    PermCheck: 100%

--- codility/PermCheck.py
+++ codility/PermCheck.py
@@ -2,16 +2,13 @@ def solution(A):
     Max = max(A)
     Min = min(A)
     A.sort()
-    l = len(A)
-    
+    l = len(A)   
     if(A[0] == 1 and l == 1 ):
-        return 1
-               
+        return 1              
     else:
         if Max - Min + 1 == l and A[0] == 1 and l == len(set(A)):
             return 1
         else:
             return 0 
-
         return 0 
 #solution([9, 5, 7, 4, 2, 6, 3, 1, 10, 8])
