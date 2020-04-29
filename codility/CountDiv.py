60d064c599fb94a5e32a0918f7c8897d0b0148f9
mykola-avram <mykola.avram@gmail.com>
2020-04-29 20:04:41 +0200

    CountDiv: 100%

---
+++ codility/CountDiv.py
@@ -0,0 +1,8 @@
+def solution(A, B, M):
+    #counter = 0; 
+    #print(A, B)
+    if (A % M == 0): 
+        print(((B // M) - (A // M)) + 1)
+    print( ((B // M) - (A // M)))
+    #print ((B // M) - (A // M))
+#solution(0, 2000000000,1)
