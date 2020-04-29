5b67d704edf7caa13850a0d9b85a199773d9cf96
mykola-avram <mykola.avram@gmail.com>
2020-04-29 16:15:51 +0200

    PermCheck: 83%

--- codility/PermCheck.py
+++ codility/PermCheck.py
@@ -1,18 +1,23 @@
 def solution(A):
     Max = max(A)
     Min = min(A)
-    #A = list(set(A))
+    A.sort()
+    #print(A)
     l = len(A)
-    if(l == 1 and A[0] == 1):
-        print(1)
+    
+    if(A[0] == 1 and l == 1 ):
+        print('sdf',1)
         return 1
-        
+               
     else:
-        if Max - Min + 1 == len(A) and Max != Min :
-            print(1)
+        if Max - Min + 1 == l and A[0] == 1:
+            print('sdsf', 1)
             return 1
         else:
             print(0)
             return 0 
+        print(0)
+        return 0 
+
     
-solution([3,2])
+solution([4,3,1,2])
