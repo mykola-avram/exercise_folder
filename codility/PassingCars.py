50a799e2fd5f6b8c4d5302bde81d095f25615fe3
mykola-avram <mykola.avram@gmail.com>
2020-05-03 19:10:36 +0200

    PassingCars: 50%

--- codility/PassingCars.py
+++ codility/PassingCars.py
@@ -1,20 +1,14 @@
 def solution(A):
-    n = len(A)
-    count_of_zero=A.count(0)
-    count_of_ones=A.count(1)
+    l = len(A)
     pairs =  1000000000
-    if count_of_ones+count_of_zero > pairs:
-        return -1
+    count_of_zero = 0
+    for i, y in enumerate(A):
+        if y == 0 and l != 1:           
+            count_of_zero += A[i:].count(1)
+            #print(count_of_zero)
+    if count_of_zero > pairs:
+          return -1
     else:
-        return count_of_ones+count_of_zero
-    # P = [0] * (n + 1)
-    # for k in range(1, n + 1):
-    #     P[k] = P[k - 1] + A[k - 1]
-    # print(P)
-    
-    
-    #count_total(P, x, y):
-    # print(P[count_of_ones + 1] - P[count_of_zero])
-    # print(count_of_zero,count_of_ones)
-
+          return count_of_zero
+ 
 solution([0,1,0,1,1])
