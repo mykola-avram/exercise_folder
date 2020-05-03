cba09b5ba2e82716757a1d753d10f9cc6c6a34d3
mykola-avram <mykola.avram@gmail.com>
2020-05-03 22:20:16 +0200

    PassingCars: 100%

--- codility/PassingCars.py
+++ codility/PassingCars.py
@@ -1,14 +1,15 @@
 def solution(A):
-    l = len(A)
+
     pairs =  1000000000
-    count_of_zero = 0
-    for i, y in enumerate(A):
-        if y == 0 and l != 1:           
-            count_of_zero += A[i:].count(1)
-            #print(count_of_zero)
-    if count_of_zero > pairs:
-          return -1
-    else:
-          return count_of_zero
+    cars = 0
+    cross = 0
+    for i in A:
+        if i == 0:
+            cars += 1
+        else:
+            cross += cars
+            if cross > pairs:
+                return -1
+    print(cross)
  
 solution([0,1,0,1,1])
