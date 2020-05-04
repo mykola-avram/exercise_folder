36efaf6753e6eee40407e4f316a16e0eb60c3935
mykola-avram <mykola.avram@gmail.com>
2020-05-04 10:07:12 +0200

    Distinct: 100%

--- codility/Distinct.py
+++ codility/Distinct.py
@@ -1,12 +1,6 @@
 def solution(A):
-    # write your code in Python 3.6
-    unique_list = [] 
-   
-    for x in A: 
-        # check if exists in unique_list or not 
-        if x not in unique_list: 
-            unique_list.append(x) 
-    # print list 
-    return len(unique_list)
+        # write your code in Python 3.6 
+        A = set(A)
+        return len(A)
     
 solution([2,1,1,2,3,1,-100])
