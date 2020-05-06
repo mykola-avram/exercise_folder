06db82934334280ab005f44301f82164e7361658
mykola-avram <mykola.avram@gmail.com>
2020-05-06 17:10:42 +0200

    Triangle: 100%

--- codility/Triangle.py
+++ codility/Triangle.py
@@ -11,19 +11,6 @@ def solution (A):
                 break
         return 0
     else:
-        return 0
-    
+        return 0   
 #solution([10, 50, 5, 1])
 #solution([0,1,2,3,4,5]) 
-# is triangular if 0 ≤ P < Q < R < N      5 8 10
-# 1 10 5
-# P Q R 
-# 5 1 
-# 1 1 5  
-# A[P] + A[Q] > A[R],
-# A[Q] + A[R] > A[P],
-# A[R] + A[P] > A[Q].
-# For example, consider array A such that:
-
-#   A[0] = 10    A[1] = 2    A[2] = 5
-#   A[3] = 1     A[4] = 8    A[5] = 20
