0b1d776057adb4932a5012df97ce55fd6d6fc558
mykola-avram <mykola.avram@gmail.com>
2020-05-09 09:35:52 +0200

    NumberOfDiscIntersections: 100%

--- codility/NumberOfDiscIntersections.py
+++ codility/NumberOfDiscIntersections.py
@@ -1,26 +1,21 @@
 def solution(A):
-    pairs = 10000000
+    l = len(A)           
+    Max = [0]*l  
+    Min = [0]*l   
+    for i in range(0, l):
+        Max[i] = i + A[i]
+        Min[i] = i - A[i]
+    Max.sort()
+    Min.sort()
+    range_lower_index = 0
     count = 0
-    for i in A:
-        for y in A[i+1:]:
-            print(i,y)
-    # for i,j in enumerate(A):        
-    #         start = i-j
-    #         end = i+j
-    #         print(start,end, i, j)
-            #string_list.extend([[start,end]])
-    #print(string_list)
-    # for k, x in enumerate(string_list):
-    #     for y in string_list[k+1:]:
-    #         #print('47: ', x,y)
-    #         if x[-1] >= y[0]:
-    #             count = count + 1
-    #             if count > pairs:
-    #                 count = -1
-    #                 break
-    # print(count)
-    # return count
-
+    for Range in range(0, l):
+        while range_lower_index < l and Max[Range] >= Min[range_lower_index]:
+            range_lower_index += 1
+        count += range_lower_index - Range -1
+        if count > 10000000:
+            return -1
+    return count
 #solution([1, 2147483647, 0])
 #solution([1, 10, 100, 1])  
 solution([1,5,2,1,4,0])
