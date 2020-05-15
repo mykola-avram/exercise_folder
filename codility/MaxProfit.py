ed08088bda75eee36a3492bcdf349502c0dc99c2
mykola-avram <mykola.avram@gmail.com>
2020-05-15 14:31:50 +0200

    MaxProfit: 88%

--- codility/MaxProfit.py
+++ codility/MaxProfit.py
@@ -1,16 +1,18 @@
-def solution(A):
-    # write your code in Python 3.6
-    #A.reverse()
-    l = len(A)
-    if l > 0:
-        profit = 0 
-        Max = max(A)
-        Min = min(A)
-        start = A.index(Min)
-        print(max(A[start:]) -  min(A), max(A[start:])) 
-    else:
-        return 0 
+def solution(A): 
+    l = len(A) 
+    if l > 0:  
+        max_diff = A[1] - A[0] 
+        min_element = A[0] 
+        for i in range( 1, l ): 
+            if (A[i] - min_element > max_diff): 
+                max_diff = A[i] - min_element 
 
+            if (A[i] < min_element): 
+                min_element = A[i] 
+        return max_diff 
+    else:
+        return 0  
+# Driver program to test above function  
 #solution([])
 
 
